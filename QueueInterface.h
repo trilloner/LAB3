dde790bb4119207f9f71f875737a0e56ffb32274
Bogdan <bo_10@ukr.net>
2020-05-04 23:40:40 +0300

    Added documentation

--- QueueInterface.h
+++ QueueInterface.h
@@ -1,13 +1,32 @@
-//
-// Created by bo_10 on 5/4/2020.
-//
-
+/**
+ * 2nd Course K-28
+ * Lab 3a
+ * Task: Create multithreading queue
+ *
+ * Purpose: contains declaration about QueueInterface
+ *
+ *@author Bogdan Volokhonenko
+ *@version 4/5/20
+ */
 #ifndef THREAD_QUEUE_QUEUEINTERFACE_H
 #define THREAD_QUEUE_QUEUEINTERFACE_H
 
+template <class T>
 class QueueInterface{
 public:
-    virtual 
+    virtual void insert(const T& data)=0;
+
+    virtual void popFront()=0;
+
+    virtual int getSize() {return 0;}
+
+    virtual bool isEmpty(){return 0;}
+
+    virtual T getFront(){return 0;}
+
+    virtual T getBack(){return 0;}
+
+    virtual void display()=0;
 };
 
 
