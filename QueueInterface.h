3110ed5d26411c2a3ce676b147621e0f3f6d6908
Bogdan <bo_10@ukr.net>
2020-05-04 13:54:14 +0300

    Create QueueInterface.h

---
+++ QueueInterface.h
@@ -0,0 +1,15 @@
+//
+// Created by bo_10 on 5/4/2020.
+//
+
+#ifndef THREAD_QUEUE_QUEUEINTERFACE_H
+#define THREAD_QUEUE_QUEUEINTERFACE_H
+
+class QueueInterface{
+public:
+    virtual 
+};
+
+
+
+#endif //THREAD_QUEUE_QUEUEINTERFACE_H
