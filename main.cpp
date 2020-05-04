54926096353911e9c9a4a6c2314f92d43d615a35
Bogdan <bo_10@ukr.net>
2020-05-04 23:41:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,13 @@
 #include <iostream>
-#include "BlockingQueue.h"
-#include <thread>
 #include <chrono>
-//#include "Time.h"
-#include "Queue.h"
+#include "demonstration.cpp"
 using namespace std;
-
-
-void Oper(){
-    this_thread::sleep_for(chrono::milliseconds(1000));
-    cout<< "Any operations"<<endl;
-}
 
 
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
-
     chrono::high_resolution_clock now;
-  /*BlockingQueue<int> que;
-
-    bool temp;
-
-    thread first([&]{
-        que.Take();
-    });
-
-    thread second([&]{
-        que.Put(6);
-    });
-    thread third([&]{
-        cout << que.Back();
-    });
-    thread ff([&]{
-        que.Put(23);
-    });
-
-    first.join();
-    second.join();
-    third.join();
-    ff.join();
-    cout<< endl;
-    cout << que.Size();
-*/
-   Queue<int> tt;
-    cout<<endl;
-    thread first([&]{
-        tt.popFront();
-    });
-    thread second([&]{
-        tt.insert(4);
-    });
-    cout << tt.getSize() << "-siz";
-    thread third([&]{
-
-        tt.display();
-    });
-    thread fr([&]{
-        tt.insert(2);
-    });
-    thread ff([&]{
-        tt.display();
-    });
-
-
-
-
-    first.join();
-    second.join();
-    third.join();
-    fr.join();
-    ff.join();
-
-//tt.display();
-    tt.insert(3);
-
-    tt.insert(45);
-    if(!tt.isEmpty())
-        cout<<"not empty"<<endl;
-    cout << tt.getFront()<<endl;
-    cout<< tt.getBack()<<endl;
-    tt.display();
-    tt.display();
-    cout << tt.getSize();
-
+    demo();
     auto end= std::chrono::high_resolution_clock::now();
     std::chrono::duration<float >duration=end-start;
     std::cout<<"Duration: "<< duration.count() << std::endl;
